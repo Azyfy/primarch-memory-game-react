17b76a795a07d9706998aec973b351594ba45f9f
Azzryel <AzryelShroud@gmail.com>
2021-02-05 19:20:34 +0100

    move images to public

--- src/components/Primarchs.js
+++ src/components/Primarchs.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 
 const Primarchs = () => {
+    const [sketch, setSketch] = useState("./images/Primarch1.jpg")
 
 
 
     return (
+        <div>
         <h1> Primarchs </h1>
+        <img src={sketch} />
+        </div>
     );
 };
 
