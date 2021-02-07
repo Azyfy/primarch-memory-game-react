3b787263242712c256ae859628a4c7485393790c
Azzryel <AzryelShroud@gmail.com>
2021-02-07 18:24:09 +0100

    add a tooltip

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ function App() {
       <header> 
         <p id="warhammer"> Warhammer 40k </p>
         <h1> Primarchs </h1>
-        <p> Memory Game </p>
+        <div> Memory Game  <div className="tooltip">&#9432;
+          <span className="tooltiptext"> Score by clicking each time on a different primarch. </span></div> 
+        </div>
       </header>
 
       <Primarchs></Primarchs>
