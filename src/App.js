12f427181efa5b2a760e017bed761768a0dc6274
Azzryel <AzryelShroud@gmail.com>
2021-02-07 14:29:55 +0100

    add header

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import "./styles/reset.css";
 import "./styles/App.css";
 
 function App() {
+ 
   return (
     <div className="App">
+      <header> 
+        <p id="warhammer"> Warhammer 40k </p>
+        <h1> Primarchs </h1>
+        <p> Memory Game </p>
+      </header>
+
       <Primarchs></Primarchs>
     </div>
   );
